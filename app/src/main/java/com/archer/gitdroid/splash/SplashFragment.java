2aa849a0103ab1530311dbd29759125076d91687
seven <377289596@qq.com>
2016-07-26 15:56:45 +0800

    实现启动页的动画效果

--- app/src/main/java/com/archer/gitdroid/splash/SplashFragment.java
+++ app/src/main/java/com/archer/gitdroid/splash/SplashFragment.java
@@ -1,5 +1,7 @@
 package com.archer.gitdroid.splash;
 
+import android.animation.ArgbEvaluator;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +9,9 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.archer.gitdroid.R;
 
@@ -24,14 +29,28 @@ public class SplashFragment extends Fragment {
     ViewPager viewPager;
     @Bind(R.id.indicator)
     CircleIndicator indicator;
+    @Bind(R.id.layoutPhone)
+    FrameLayout layoutPhone;
+    @Bind(R.id.content)
+    FrameLayout content;
+    @Bind(R.id.ivPhoneFont)
+    ImageView ivPhoneFont;
 
     private ViewPagerAdapter adapter;
+
+    private int colorGreen;
+    private int colorRed;
+    private ArgbEvaluator argbEvaluator;
+
+    private int widthScreen = 0;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_splash_pager, container, false);
         ButterKnife.bind(this, view);
+        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        widthScreen = wm.getDefaultDisplay().getWidth();
         return view;
     }
 
@@ -43,6 +62,11 @@ public class SplashFragment extends Fragment {
         viewPager.setAdapter(adapter);
         indicator.setViewPager(viewPager);
 
+        argbEvaluator = new ArgbEvaluator();
+
+        colorGreen = getResources().getColor(R.color.colorGreen);
+        colorRed = getResources().getColor(R.color.colorRed);
+
         viewPager.addOnPageChangeListener(pagerColorListener);
         viewPager.addOnPageChangeListener(pagerPhoneListener);
 
@@ -51,6 +75,14 @@ public class SplashFragment extends Fragment {
     private ViewPager.OnPageChangeListener pagerColorListener = new ViewPager.OnPageChangeListener() {
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            if (position == 0) {
+                int color = (int) argbEvaluator.evaluate(positionOffset, colorGreen, colorRed);
+                content.setBackgroundColor(color);
+                ivPhoneFont.setAlpha(positionOffset);
+            } else {
+                content.setBackgroundColor(colorRed);
+            }
 
         }
 
@@ -68,6 +100,22 @@ public class SplashFragment extends Fragment {
     private ViewPager.OnPageChangeListener pagerPhoneListener = new ViewPager.OnPageChangeListener() {
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            if (position == 0) {
+
+                float scale = 0.3f + (positionOffset * 0.7f);
+                layoutPhone.setScaleX(scale);
+                layoutPhone.setScaleY(scale);
+
+                float x = -360 + (360 * positionOffset);
+                layoutPhone.setTranslationX(x);
+
+            }
+
+            if (position == 1) {
+                layoutPhone.setTranslationX(widthScreen * -positionOffset);
+            }
+
 
         }
 
